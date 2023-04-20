42c3218edd63d28ed60551b8c95b6eb01a41c7dc
vishalicious213 <vishalicious213@gmail.com>
2023-04-19 23:50:49 -0700

    clicking on 'mountain 1' and 'mountain 2' buttons changes mountain image in team section

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Los Angeles Mountains")
+const mountain = document.getElementById("mountain")
+const mountain1 = document.getElementById("mtn-1")
+const mountain2 = document.getElementById("mtn-2")
+
+mountain1.addEventListener("click", function() {
+    mountain.src = `/img/background-3.jpg`
+})
+
+mountain2.addEventListener("click", function() {
+    mountain.src = `/img/background-4.jpg`
+})
