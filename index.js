1f490d85b73708db49d595374a17d0e6356c600e
vishalicious213 <vishalicious213@gmail.com>
2023-04-20 14:03:56 -0700

    made burger menu operational

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mountain = document.getElementById("mountain")
 const mountain1 = document.getElementById("mtn-1")
 const mountain2 = document.getElementById("mtn-2")
+const mobileNav = document.getElementById("mobile-nav")
+const mobileMenuItems = document.getElementById("mobile-menu-items")
 
 mountain1.addEventListener("click", function() {
     mountain.src = `/img/background-3.jpg`
@@ -8,4 +10,9 @@ mountain1.addEventListener("click", function() {
 
 mountain2.addEventListener("click", function() {
     mountain.src = `/img/background-4.jpg`
+})
+
+mobileNav.addEventListener("click", function() {
+    console.log("clicked")
+    mobileMenuItems.classList.toggle("display-block")
 })
